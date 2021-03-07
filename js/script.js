6b0c695fff0ce81b0c26dcf3c3ba975044907c37
Anna <67719118+brasspetals@users.noreply.github.com>
2021-03-07 15:04:36 -0600

    Add testing for some pledge form submit functionality, reorganize js
    Very rough, will be heavily reworked

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
+//////////////
+// Overlay //
+////////////
+const overlay = document.querySelector(".overlay");
+
+// Close mobile menu and modals when clicking outside them
+overlay.addEventListener("click", function () {
+  if (overlay.classList.contains("overlay--modal")) {
+    closeModal();
+  } else {
+    toggleMenu();
+  }
+});
+
 //////////////////
 // Mobile Menu //
 ////////////////
 const btnMenu = document.querySelector(".btn--menu");
 const btnMenuIcon = document.querySelector(".menu-icon");
 const nav = document.querySelector(".navigation__list");
-const overlay = document.querySelector(".overlay");
 
 function toggleMenu() {
   nav.classList.toggle("navigation__list--hidden");
@@ -19,6 +32,8 @@ function toggleMenu() {
     btnMenu.setAttribute("aria-expanded", "true");
   }
 }
+
+btnMenu.addEventListener("click", toggleMenu);
 
 //////////////////////
 // Bookmark Button //
@@ -35,6 +50,8 @@ function bookmarkProject() {
   }
 }
 
+bookmark.addEventListener("click", bookmarkProject);
+
 /////////////
 // Modals //
 ///////////
@@ -46,38 +63,78 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 // Open selection modal, apply overlay
 function openModal() {
   modalPledges.classList.remove("modal--hidden");
-  overlay.classList.toggle("overlay--hidden");
+  overlay.classList.remove("overlay--hidden");
   overlay.classList.add("overlay--modal");
 }
 
 // Close selection modal, remove overlay
 function closeModal() {
   modalPledges.classList.add("modal--hidden");
-  overlay.classList.toggle("overlay--hidden");
+  overlay.classList.add("overlay--hidden");
   overlay.classList.remove("overlay--modal");
 }
 
-//////////////////////
-// Event Listeners //
-////////////////////
-btnMenu.addEventListener("click", toggleMenu);
 btnPrimary.addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
-bookmark.addEventListener("click", bookmarkProject);
 
 // All pledge buttons open selection modal
 btnPledge.forEach((item) => {
-  item.addEventListener("click", (event) => {
+  item.addEventListener("click", () => {
+    // Mark corresponding modal radio input as checked
     document.getElementById(`reward-${item.id}`).checked = true;
     openModal();
   });
 });
 
-// Close mobile menu and modals when clicking outside them
-overlay.addEventListener("click", function () {
-  if (overlay.classList.contains("overlay--modal")) {
+/////////////////
+// Testing Area
+const amountGoal = 100000;
+let amountBacked = 89914;
+let totalBackers = 5007;
+let numBamboo = 101;
+let numBlack = 64;
+let numMahogany = 0;
+const formBtns = document.querySelectorAll(".btn--modal-form");
+
+formBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // prevents page refresh
+    e.preventDefault();
+
+    // decrement remaining selected reward by one (if applicable)
+    console.log(btn.id);
+    if (btn.id === "submit-bamboo") {
+      updateBamboo();
+    } else if (btn.id === "submit-black") {
+      updateBlack();
+    }
+
+    // add one to total number of backers
+    totalBackers++;
+    document.getElementById(
+      "num-backers"
+    ).innerHTML = totalBackers.toLocaleString();
+
+    // add amount pledged to total amount backed
+
+    // update slider to reflect new amount backed
+
     closeModal();
-  } else {
-    toggleMenu();
-  }
+  });
 });
+
+// subtract one from number of bamboo stands remaining, update DOM
+function updateBamboo() {
+  numBamboo--;
+  document.querySelectorAll(".numBamboo").forEach((item) => {
+    item.innerHTML = numBamboo;
+  });
+}
+
+// subtract one from number of black stands remaining, update DOM
+function updateBlack() {
+  numBlack--;
+  document.querySelectorAll(".numBlack").forEach((item) => {
+    item.innerHTML = numBlack;
+  });
+}

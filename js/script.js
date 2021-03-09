f38d97e3a70af7a23dd016104d7187f697b21231
Anna <67719118+brasspetals@users.noreply.github.com>
2021-03-09 13:42:29 -0600

    Add first proper draft of pledge submission functionality
    - also add form reset on selection modal close

--- js/script.js
+++ js/script.js
@@ -19,11 +19,13 @@ const btnMenu = document.querySelector(".btn--menu");
 const btnMenuIcon = document.querySelector(".menu-icon");
 const nav = document.querySelector(".navigation__list");
 
+// Toggle mobile nav and overlay visibility
 function toggleMenu() {
   nav.classList.toggle("navigation__list--hidden");
   overlay.classList.toggle("overlay--hidden");
   document.body.classList.toggle("disable-scroll");
 
+  // Change menu button icon and aria-expanded
   if (nav.classList.contains("navigation__list--hidden")) {
     btnMenuIcon.src = "./images/icon-hamburger.svg";
     btnMenu.setAttribute("aria-expanded", "false");
@@ -40,6 +42,7 @@ btnMenu.addEventListener("click", toggleMenu);
 ////////////////////
 const bookmark = document.querySelector(".bookmark");
 
+// Toggle bookmark active styles
 function bookmarkProject() {
   bookmark.classList.toggle("bookmark--active");
 
@@ -59,6 +62,16 @@ const btnPrimary = document.querySelector(".btn--primary");
 const btnPledge = document.querySelectorAll(".btn--pledge");
 const modalPledges = document.querySelector(".modal");
 const btnCloseModal = document.querySelector(".btn--close-modal");
+const amountGoal = 100000;
+let totalBacked = 89914;
+let totalBackers = 5007;
+let numBamboo = 101;
+let numBlack = 64;
+let numMahogany = 0;
+const formBtns = document.querySelectorAll(".btn--modal-form");
+
+////////////////////////////////
+// Selection Modal Open/Close
 
 // Open selection modal, apply overlay
 function openModal() {
@@ -67,46 +80,40 @@ function openModal() {
   overlay.classList.add("overlay--modal");
 }
 
-// Close selection modal, remove overlay
+// Close selection modal, remove overlay, reset forms
 function closeModal() {
   modalPledges.classList.add("modal--hidden");
   overlay.classList.add("overlay--hidden");
   overlay.classList.remove("overlay--modal");
+
+  document.querySelectorAll(".pledge__modal-form").forEach((form) => {
+    form.reset();
+  });
 }
 
-btnPrimary.addEventListener("click", openModal);
-btnCloseModal.addEventListener("click", closeModal);
-
 // All pledge buttons open selection modal
-btnPledge.forEach((item) => {
-  item.addEventListener("click", () => {
+btnPledge.forEach((btn) => {
+  btn.addEventListener("click", () => {
     // Mark corresponding modal radio input as checked
-    document.getElementById(`reward-${item.id}`).checked = true;
+    document.getElementById(`reward-${btn.dataset.group}`).checked = true;
     openModal();
   });
 });
 
-/////////////////
-// Testing Area
-const amountGoal = 100000;
-let amountBacked = 89914;
-let totalBackers = 5007;
-let numBamboo = 101;
-let numBlack = 64;
-let numMahogany = 0;
-const formBtns = document.querySelectorAll(".btn--modal-form");
+btnPrimary.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+
+////////////////////////////////
+// Submitting a Pledge
 
 formBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     // prevents page refresh
     e.preventDefault();
 
-    // decrement remaining selected reward by one (if applicable)
-    console.log(btn.id);
-    if (btn.id === "submit-bamboo") {
-      updateBamboo();
-    } else if (btn.id === "submit-black") {
-      updateBlack();
+    // decrement remaining selected reward by one (if applicable) and update DOM
+    if (btn.dataset.group != "noreward") {
+      updateReward(btn);
     }
 
     // add one to total number of backers
@@ -115,26 +122,45 @@ formBtns.forEach((btn) => {
       "num-backers"
     ).innerHTML = totalBackers.toLocaleString();
 
-    // add amount pledged to total amount backed
+    // add amount pledged to total amount backed & update silder percentage
+    updateTotalBacked(btn);
 
-    // update slider to reflect new amount backed
-
+    // close selection modal and open thank you modal
     closeModal();
   });
 });
 
-// subtract one from number of bamboo stands remaining, update DOM
-function updateBamboo() {
-  numBamboo--;
-  document.querySelectorAll(".numBamboo").forEach((item) => {
-    item.innerHTML = numBamboo;
-  });
+function updateReward(btn) {
+  const reward = btn.dataset.group;
+
+  if (reward == "bamboo") {
+    numBamboo--;
+    document.querySelectorAll(".numBamboo").forEach((item) => {
+      item.innerHTML = numBamboo;
+    });
+  } else if (reward == "black") {
+    numBlack--;
+    document.querySelectorAll(".numBlack").forEach((item) => {
+      item.innerHTML = numBlack;
+    });
+  }
 }
 
-// subtract one from number of black stands remaining, update DOM
-function updateBlack() {
-  numBlack--;
-  document.querySelectorAll(".numBlack").forEach((item) => {
-    item.innerHTML = numBlack;
-  });
+function updateTotalBacked(btn) {
+  const amountPledged = parseInt(
+    document.getElementById(`amount-${btn.dataset.group}`).value
+  );
+
+  totalBacked += amountPledged;
+
+  document.getElementById(
+    "total-backed"
+  ).innerHTML = `$${totalBacked.toLocaleString()}`;
+
+  // update slider width to new percentage of total backed
+  const percentageBacked = Math.floor((totalBacked / amountGoal) * 100);
+
+  document.querySelector(
+    ".statistics__slider-inner"
+  ).style.width = `${percentageBacked}%`;
 }
